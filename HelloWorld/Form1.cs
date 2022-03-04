089abd95cf2293933020f255c39c9bd1b321199d
jahfal21 <jahfalramadhan4@gmail.com>
2022-03-04 16:45:09 +0700

    TP Mod 2 KPL commit

---
+++ HelloWorld/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nama = textBox1.Text;
+            MessageBox.Show("Hallo " + nama + "! \nAku tahu kamu lelah, Semangat terus ya!!",
+                "Pesan dari dia");
+        }
+    }
+}
